6757e0adf7d523ffc605423d8dcd64191574d409
Manuel Woelker <github@manuel.woelker.org>
2015-08-24 17:57:45 +0200

    feat(server): simple nickel static file server

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 #![allow(dead_code)]
+#[macro_use]
+extern crate nickel;
+
+use nickel::{Nickel, HttpRouter, StaticFilesHandler};
 
 fn main() {
-    println!("Hello, world!");
+    let mut server = Nickel::new();
+    server.utilize(StaticFilesHandler::new("target/doc/"));
+//    server.get("**", middleware!("Hello World"));
+    server.listen("127.0.0.1:3000");
+    println!("Listening on http://localhost:3000/");
 }
