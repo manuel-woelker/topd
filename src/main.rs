1de44da46fda7114b8ba50800e007fc8b1be6f7d
Manuel Woelker <github@manuel.woelker.org>
2015-08-31 21:57:49 +0200

    chore: tab-indent

--- src/main.rs
+++ src/main.rs
@@ -11,24 +11,24 @@ use std::collections::HashMap;
 use nickel::{Nickel, HttpRouter, StaticFilesHandler, MediaType};
 
 fn main() {
-    let mut server = Nickel::new();
-    let mut router = Nickel::router();
+	let mut server = Nickel::new();
+	let mut router = Nickel::router();
+	
+	router.get("/api/system-	metrics", middleware! { |_, mut response|
+		response.set(MediaType::Json);
+		let loadavg = procinfo::loadavg().unwrap();
+		let mut result = HashMap::new();
+		let mut loadavg_map = HashMap::new();
+		loadavg_map.insert("load_avg_1_min", loadavg.load_avg_1_min);
+		loadavg_map.insert("load_avg_5_min", loadavg.load_avg_5_min);
+		loadavg_map.insert("load_avg_10_min", loadavg.load_avg_10_min);
+		result.insert("loadavg", loadavg_map);
+		serde_json::to_string(&result).unwrap()
+	});
 
-    router.get("/api/system-metrics", middleware! { |_, mut response|
-        response.set(MediaType::Json);
-        let loadavg = procinfo::loadavg().unwrap();
-        let mut result = HashMap::new();
-        let mut loadavg_map = HashMap::new();
-        loadavg_map.insert("load_avg_1_min", loadavg.load_avg_1_min);
-        loadavg_map.insert("load_avg_5_min", loadavg.load_avg_5_min);
-        loadavg_map.insert("load_avg_10_min", loadavg.load_avg_10_min);
-        result.insert("loadavg", loadavg_map);
-        serde_json::to_string(&result).unwrap()
-    });
-
-    server.utilize(router);
-    server.utilize(StaticFilesHandler::new("target/doc/"));
-//    server.get("**", middleware!("Hello World"));
-    server.listen("127.0.0.1:3000");
-    println!("Listening on http://localhost:3000/");
+	server.utilize(router);
+	server.utilize(StaticFilesHandler::new("target/doc/"));
+//	  server.get("**", middleware!("Hello World"));
+	server.listen("127.0.0.1:3000");
+	println!("Listening on http://localhost:3000/");
 }
