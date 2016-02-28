812b9bd33cfaf07a85e9d060e023f904472fb921
Manuel Woelker <github@manuel.woelker.org>
2016-02-28 10:03:07 +0100

    refactor(UI/History): generic history component

--- ui/src/app/TopApp.js
+++ ui/src/app/TopApp.js
@@ -4,11 +4,24 @@ import {Navbar, Nav, NavbarBrand, NavItem, Grid, Row} from "react-bootstrap";
 
 import LoadAvgComponent from "../load/LoadAvgComponent.js";
 import CpuUsageComponent from "../cpu/CpuUsageComponent.js";
-import CpuHistoryComponent from "../cpu/CpuHistoryComponent.js";
+import HistoryComponent from "../history/HistoryComponent.js";
 
 
 export default connect(state => state)(React.createClass({
 	render() {
+		var cpuMetrics = [
+			{
+				strokeStyle: "#0d3c55",
+				values: this.props.cpuHistory.user
+			},	{
+				strokeStyle: "#c02e1d",
+				values: this.props.cpuHistory.system
+			},	{
+				strokeStyle: "#76772a",
+				values: this.props.cpuHistory.other
+			}
+
+		];
 		return <div>
 			<Navbar fluid>
 				<NavbarBrand><a href="#">topd <b>{this.props.systemInfo.hostname}</b></a></NavbarBrand>
@@ -22,7 +35,8 @@ export default connect(state => state)(React.createClass({
 					<Row>
 						<LoadAvgComponent loadavg={this.props.systemMetrics.loadavg}/>
 						<CpuUsageComponent cpu_usage={this.props.systemMetrics.cpu_usage}/>
-						<CpuHistoryComponent cpuHistory={this.props.cpuHistory}/>
+						<h2>CPU History</h2>
+						<HistoryComponent metrics={cpuMetrics}/>
 					</Row>
 				</Grid>
 			</div>
--- ui/src/history/HistoryComponent.js
+++ ui/src/history/HistoryComponent.js
@@ -8,7 +8,8 @@ export default React.createClass({
 		var height = canvas.height;
 		var width = canvas.width;
 		ctx.clearRect(0, 0, width, height); // clear canvas
-		let array = this.props.cpuHistory.user;
+		var metrics = this.props.metrics;
+        let array = metrics[0].values;
 		let xScale = width / array.length;
 		let yScale = width;
 		ctx.lineWidth = 5 / Math.max(width, height);
@@ -17,12 +18,11 @@ export default React.createClass({
 		ctx.translate(-1, 0);
 		ctx.scale(1, -1);
 
-		ctx.strokeStyle = "#0d3c55";
-		drawLine(this.props.cpuHistory.user);
-		ctx.strokeStyle = "#c02e1d";
-		drawLine(this.props.cpuHistory.system);
-		ctx.strokeStyle = "#76772a";
-		drawLine(this.props.cpuHistory.other);
+		for(var i = 0; i < metrics.length; i++) {
+			var metric = metrics[i];
+			ctx.strokeStyle = metric.strokeStyle;
+			drawLine(metric.values);
+		}
 
 		function drawLine(array) {
 			ctx.beginPath();
@@ -30,7 +30,6 @@ export default React.createClass({
 			var lastx = -1;
 			var lasty = 0;
 			for (var i = 1; i < array.length; i++) {
-//			ctx.lineTo(i, array[i]);
 				ctx.bezierCurveTo(i - 0.5, lasty, i - 0.5, array[i], i, array[i]);
 				lastx = i;
 				lasty = array[i];
@@ -44,9 +43,7 @@ export default React.createClass({
 	},
 
 	render() {
-		let cpuHistory = this.props.cpuHistory || {};
 		return <div>
-			<h2>CPU History</h2>
 			<canvas ref="canvas" style={{border: "1px solid grey"}} height="400" width="800"/>
 		</div>
 	}
